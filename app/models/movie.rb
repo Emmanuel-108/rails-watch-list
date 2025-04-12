c43106d39c9690ca41e099defddba7655bf717f9
Emanuel Flores <flores.emanuel.108@gmail.com>
2025-04-12 14:32:43 -0400

    movies site with clean database

---
+++ app/models/movie.rb
@@ -0,0 +1,18 @@
+class Movie < ApplicationRecord
+  # has_many :bookmarks, dependent: :restrict_with_error
+  has_many :bookmarks
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true, uniqueness: true
+  validates :poster_url, presence: true, format: URI::regexp(%w[http https])
+  validates :rating, presence: true, numericality: { only_integer: false, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
+  before_validation :set_defaults
+
+  private
+
+  def set_defaults
+    self.poster_url ||= "https://example.com/default-poster.jpg"
+    self.rating ||= 5.0
+  end
+end
